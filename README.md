fc17ea98cfe4697e401eb8f82fb02c58b466543a
astrong19 <asastrong19@gmail.com>
2017-08-15 12:00:33 -0400

    add geojson for follow along, add gitignore

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # example_gfw_api_calls
-Temporary Repo to showcase some simple API calls for the FOSS4G Conference
+Some simple example API calls for the FOSS4G Conference
+
+Some follow along data too
--- geostore/geojson_to_geostore.py
+++ geostore/geojson_to_geostore.py
@@ -9,14 +9,17 @@ def create_geostore(geojson):
 
     r = requests.post(api_path, json=payload)
 
-    geostore_id = r.json()["data"]["id"]
+    print r.json()
 
-    print geostore_id
+    # geostore_id = r.json()["data"]["id"]
+    #
+    # print geostore_id
 
 if __name__ == "__main__":
 
-    geojson = ({"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":
-    {"type":"Polygon","coordinates":[[[-5.273512601852417,42.81137220349083],[-5.273512601852417,42.811803118457306]
-    ,[-5.272732079029083,42.811803118457306],[-5.272732079029083,42.81137220349083],[-5.273512601852417,42.81137220349083]]]}}]})
-
+    geojson = ({"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":
+    [[[-70.0711,-13.0029],[-70.0794,-13.0323],[-70.0372,-13.0434],[-69.9826,-13.0494],[-69.9105,-13.0571],
+    [-69.8936,-13.0397],[-69.9005,-13.022],[-69.9441,-13.0123],[-69.9654,-13.0046],[-70.0018,-13.0029],
+    [-70.0299,-13.0046],[-70.0598,-13.0012],[-70.0711,-13.0029]]]}}]})
+    
     create_geostore(geojson)
